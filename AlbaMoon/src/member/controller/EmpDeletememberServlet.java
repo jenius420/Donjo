099f67a0b98cf2e26b4006a34b3e51e82ff45d51
kj1kj2kj3 <kjhstan@gmail.com>
2019-08-15 10:17:34 +0900

    1

--- AlbaMoon/src/member/controller/EmpDeletememberServlet.java
+++ AlbaMoon/src/member/controller/EmpDeletememberServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class PersonJoinMemberServlet
+ * Servlet implementation class EmpDeletememberServlet
  */
-@WebServlet("/personJoinMember.me")
-public class PersonJoinMemberServlet extends HttpServlet {
+@WebServlet("/empDeletemember.me")
+public class EmpDeletememberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public PersonJoinMemberServlet() {
+    public EmpDeletememberServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +26,8 @@ public class PersonJoinMemberServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		request.getRequestDispatcher("views/member/personJoinMember.jsp").forward(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
